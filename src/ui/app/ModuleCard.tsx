098c2532af30acbcd48db99a23698906c18e6a73
RyanMai <hoangmai1070@gmail.com>
2024-11-21 20:20:57 +1100

    migrated to grid for overall layout, removed redundant divs

--- src/ui/app/ModuleCard.tsx
+++ src/ui/app/ModuleCard.tsx
@@ -1,22 +1,14 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
 import React from "react";
 
-export const ModuleCard = (): JSX.Element => {
-  return (
-    <div className="flex w-[200px] h-[100px] items-center relative bg-[#d3cd85] rounded-[20px] overflow-hidden">
-      <div className="flex w-[100px] items-center justify-center relative self-stretch">
-        <div className="relative w-20 h-20 bg-[#989898]" />
-      </div>
+interface Props {
+	className?: string;
+}
 
-      <div className="flex items-center justify-center relative flex-1 self-stretch grow">
-        <div className="relative w-fit font-single-line-body-base font-[number:var(--single-line-body-base-font-weight)] text-black text-[length:var(--single-line-body-base-font-size)] tracking-[var(--single-line-body-base-letter-spacing)] leading-[var(--single-line-body-base-line-height)] whitespace-nowrap [font-style:var(--single-line-body-base-font-style)]">
-          HEADING
-        </div>
-      </div>
-    </div>
-  );
+export const ModuleCard = ({ className }: Props): JSX.Element => {
+	return (
+		<div className={`flex w-[90%] h-[100px] items-center pl-4 bg-[#d3cd85] rounded-[20px] ${className}`}>
+			<div className="w-20 h-20 bg-[#989898]" />
+			<p className="text-black text-lg flex-1 text-center">Name</p>
+		</div>
+	);
 };
--- src/ui/app/Timeline.tsx
+++ src/ui/app/Timeline.tsx
@@ -6,21 +6,17 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 import React from "react";
 
 interface Props {
-  className: any;
+	className: string;
 }
 
 export const Timeline = ({ className }: Props): JSX.Element => {
-  return (
-    // outer box
-    <div
-      className={`flex flex-col h-200px items-start justify-center gap-2.5 p-[15px] relative border border-solid border-black ${className}`}
-    >
-        {/* // inner shadow box */}
-      <div className="relative flex-1 self-stretch w-full grow bg-white rounded-[14px] shadow-[inset_0px_4px_4px_#00000040] overflow-x-scroll" />
-
-      <div className="flex h-px items-center justify-end pl-0 pr-[26px] pt-0 pb-[158px] relative self-stretch w-full mb-[-1.00px]">
-        <div className="relative w-[50px] h-[50px] mt-[-104.00px] mb-[-103.00px] bg-[#c8c8c8] rounded-[80px]" />
-      </div>
-    </div>
-  );
+	return (
+		/* outer box */
+		<div
+			className={`h-200px p-[15px] relative ${className}`}
+		>
+			{/* // inner shadow box */}
+			<div className="h-full w-full grow bg-white rounded-[14px] shadow-[inset_0px_4px_4px_#00000040] overflow-x-scroll" />
+		</div>
+	);
 };
