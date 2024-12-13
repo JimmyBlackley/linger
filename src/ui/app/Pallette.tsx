1916a1a4bb179b3022746e42850537ae834a9403
RyanMai <hoangmai1070@gmail.com>
2024-12-13 17:12:52 +1100

    added basic ContentArea

--- src/ui/app/Pallette.tsx
+++ src/ui/app/Pallette.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import React, { useState } from "react";
 import { motion } from "motion/react";
-import { ModuleCard } from "./ModuleCard";
+import ModuleCard from "./ModuleCard";
 import { v4 as uuidv4 } from "uuid";
 import DropdownButton from "./DropdownButton";
 
@@ -11,16 +9,16 @@ interface Props {
 }
 function Pallette({ className }: Props): JSX.Element {
 	let defaultModuleList = [
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
-		{ id: uuidv4() },
+		{ id: uuidv4(), name: "Module 1" },
+		{ id: uuidv4(), name: "Module 2" },
+		{ id: uuidv4(), name: "Module 3" },
+		{ id: uuidv4(), name: "Module 4" },
+		{ id: uuidv4(), name: "Module 5" },
+		{ id: uuidv4(), name: "Module 6" },
+		{ id: uuidv4(), name: "Module 7" },
+		{ id: uuidv4(), name: "Module 8" },
+		{ id: uuidv4(), name: "Module 9" },
+		{ id: uuidv4(), name: "Module 10" },
 	];
 	const [moduleList, setModuleList] = useState(defaultModuleList);
 	const [currentActiveButton, setCurrentActiveButton] = useState("");
@@ -44,7 +42,7 @@ function Pallette({ className }: Props): JSX.Element {
 			</motion.div>
 			<div className="flex flex-col items-center gap-4 px-0 py-[15px] h-full w-full bg-gray-300 rounded-[14px] overflow-y-auto shadow-[inset_0px_4px_4px_#00000040]">
 				{moduleList.map((module) => {
-					return <ModuleCard key={module.id} className={"flex-shrink-0"} />;
+					return <ModuleCard key={module.id} className={"flex-shrink-0"} >{module.name}</ModuleCard>;
 				})}
 			</div>
 		</div>
