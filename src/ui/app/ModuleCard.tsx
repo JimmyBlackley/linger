30c0421666f53bee760f33291b2f6cc133eef318
RyanMai <hoangmai1070@gmail.com>
2024-12-13 17:08:53 +1100

    changed ModuleCard to handle dynamic content

--- src/ui/app/ModuleCard.tsx
+++ src/ui/app/ModuleCard.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 interface Props {
 	className?: string;
+	children?: React.ReactNode;
 }
 
-export const ModuleCard = ({ className }: Props): JSX.Element => {
+function ModuleCard ({ className, children }: Props): JSX.Element {
 	return (
-		<div className={`flex w-[90%] h-[100px] items-center pl-4 bg-[#d3cd85] rounded-[20px] ${className}`}>
+		<div className={`flex w-[90%] h-[100px] items-center p-4 bg-[#d3cd85] rounded-[20px] ${className}`}>
 			<div className="w-20 h-20 bg-[#989898]" />
-			<p className="text-black text-lg flex-1 text-center">Name</p>
+			<p className="text-black text-lg flex-1 text-center">{children}</p>
 		</div>
 	);
 };
+export default ModuleCard;
