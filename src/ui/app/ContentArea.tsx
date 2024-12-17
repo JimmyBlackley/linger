83dd89cc2f05d2a938b3def1d6ae882d01c3ea1c
RyanMai <hoangmai1070@gmail.com>
2024-12-17 18:46:20 +1100

    implemented drag and drop manually without animation

--- src/ui/app/ContentArea.tsx
+++ src/ui/app/ContentArea.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ModuleCard from "./ModuleCard";
 import { v4 as uuidv4 } from "uuid";
+import { motion } from "motion/react";
 
 interface Props {
 	children?: React.ReactNode;
@@ -11,12 +12,58 @@ function ContentArea({ children }: Props): JSX.Element {
 		{ id: uuidv4(), name: "This is a module filled with content" },
 		{ id: uuidv4(), name: "Test content" },
 		{ id: uuidv4(), name: "Another test content" },
-		{ id: uuidv4(), name: "Yet another test content"}
+		{ id: uuidv4(), name: "Yet another test content" },
 	]);
+	const [currentDragItemID, setCurrentDragItemID] = useState<string | null>(null);
+
+	function dragging(e: React.DragEvent) {
+		const target = e.currentTarget as HTMLElement;
+		target.style.visibility = "hidden";
+	}
+	function dragEnd(e: React.DragEvent) {
+		const target = e.currentTarget as HTMLElement;
+		target.style.visibility = "visible";
+	}
+
 	return (
-		<div className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto">
+		<div
+			className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto"
+			/* prevent default on drop behaviour to disable drag ghost snapping back to original position*/
+			onDragOver={(e) => {
+				e.preventDefault();
+			}}
+			onDrop={(e) => {
+				e.preventDefault();
+			}}
+		>
 			{content.map((item) => {
-				return <ModuleCard key={item.id} className={`w-full`}>{item.name}</ModuleCard>;
+				return (
+					<div
+						draggable
+						key={item.id}
+						className={`w-full`}
+						onDragStart={(e) => {
+							const target = e.currentTarget as HTMLElement;
+							setCurrentDragItemID(item.id);
+						}}
+						onDragOver={(e) => {
+							if (currentDragItemID) {
+								const currentDragItemIndex = content.findIndex(
+									(arrayItem) => arrayItem.id === currentDragItemID
+								);
+								const itemToReplaceIndex = content.findIndex((arrayItem) => arrayItem.id === item.id);
+								const newContent = [...content];
+								const [removedItem] = newContent.splice(currentDragItemIndex, 1);
+								newContent.splice(itemToReplaceIndex, 0, removedItem);
+								setContent(newContent);
+							}
+						}}
+						onDrag={dragging}
+						onDragEnd={dragEnd}
+					>
+						<ModuleCard className={`w-full`}>{item.name}</ModuleCard>
+					</div>
+				);
 			})}
 			{children}
 		</div>
--- src/ui/app/Timeline.tsx
+++ src/ui/app/Timeline.tsx
@@ -13,7 +13,7 @@ function Timeline ({ className }: Props): JSX.Element {
 	return (
 		/* outer box */
 		<div
-			className={`h-200px p-[15px] relative ${className}`}
+			className={`h-200px p-[15px] ${className}`}
 		>
 			{/* // inner shadow box */}
 			<div className="h-full w-full grow bg-white rounded-[14px] shadow-[inset_0px_4px_4px_#00000040] overflow-x-scroll" />
