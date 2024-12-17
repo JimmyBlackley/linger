1f26c526961829ab439a9f7ed1230ae29f512df6
RyanMai <hoangmai1070@gmail.com>
2024-12-17 20:10:34 +1100

    added drag and drop animation using framer motion

--- src/ui/app/ContentArea.tsx
+++ src/ui/app/ContentArea.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ModuleCard from "./ModuleCard";
 import { v4 as uuidv4 } from "uuid";
 import { motion } from "motion/react";
@@ -6,67 +6,77 @@ import { motion } from "motion/react";
 interface Props {
 	children?: React.ReactNode;
 }
+interface Item {
+	id: string;
+	name?: string;
+}
 
 function ContentArea({ children }: Props): JSX.Element {
-	const [content, setContent] = useState([
+	const [content, setContent] = useState<Item[]>([
 		{ id: uuidv4(), name: "This is a module filled with content" },
 		{ id: uuidv4(), name: "Test content" },
 		{ id: uuidv4(), name: "Another test content" },
 		{ id: uuidv4(), name: "Yet another test content" },
 	]);
-	const [currentDragItemID, setCurrentDragItemID] = useState<string | null>(null);
-
-	function dragging(e: React.DragEvent) {
-		const target = e.currentTarget as HTMLElement;
-		target.style.visibility = "hidden";
+	const [currentDragItem, setCurrentDragItem] = useState<Item | null>(null);
+	const [animating, setAnimating] = useState(false);
+	/* function as dragStart because browser copy current element to use as drag ghost when dragStart procs */
+	function drag(e: MouseEvent | TouchEvent | PointerEvent, item: Item) {
+		setCurrentDragItem(item);
 	}
-	function dragEnd(e: React.DragEvent) {
-		const target = e.currentTarget as HTMLElement;
-		target.style.visibility = "visible";
+	/* switching items on drag over */
+	function dragOver(e: React.DragEvent, item: Item) {
+		if (currentDragItem && !animating) {
+			setAnimating(true);
+			const currentDragItemIndex = content.findIndex((arrayItem) => arrayItem.id === currentDragItem.id);
+			const itemToReplaceIndex = content.findIndex((arrayItem) => arrayItem.id === item.id);
+			const newContent = [...content];
+			const [removedItem] = newContent.splice(currentDragItemIndex, 1);
+			newContent.splice(itemToReplaceIndex, 0, removedItem);
+			setContent(newContent);
+		}
+	}
+	function dragEnd() {
+		setCurrentDragItem(null);
+		document.removeEventListener("dragover", preventDefault);
+		document.removeEventListener("drop", preventDefault);
+	}
+	/* prevent default on drop behaviour to disable drag ghost snapping back to original position*/
+	function preventDefault(e: DragEvent) {
+		e.preventDefault();
 	}
 
 	return (
-		<div
-			className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto"
-			/* prevent default on drop behaviour to disable drag ghost snapping back to original position*/
-			onDragOver={(e) => {
-				e.preventDefault();
-			}}
-			onDrop={(e) => {
-				e.preventDefault();
-			}}
-		>
+		<motion.div className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto">
 			{content.map((item) => {
 				return (
-					<div
+					<motion.div
+						layout
+						transition={{ duration: 0.2 }}
 						draggable
 						key={item.id}
-						className={`w-full`}
+						className={`w-full ${currentDragItem && currentDragItem.id === item.id ? "invisible" : ""}`}
 						onDragStart={(e) => {
-							const target = e.currentTarget as HTMLElement;
-							setCurrentDragItemID(item.id);
+							document.addEventListener("dragover", preventDefault);
+							document.addEventListener("drop", preventDefault);
+						}}
+						onDrag={(e) => {
+							drag(e, item);
 						}}
 						onDragOver={(e) => {
-							if (currentDragItemID) {
-								const currentDragItemIndex = content.findIndex(
-									(arrayItem) => arrayItem.id === currentDragItemID
-								);
-								const itemToReplaceIndex = content.findIndex((arrayItem) => arrayItem.id === item.id);
-								const newContent = [...content];
-								const [removedItem] = newContent.splice(currentDragItemIndex, 1);
-								newContent.splice(itemToReplaceIndex, 0, removedItem);
-								setContent(newContent);
-							}
+							dragOver(e, item);
 						}}
-						onDrag={dragging}
 						onDragEnd={dragEnd}
+						onLayoutAnimationComplete={() => {
+							setAnimating(false);
+						}}
 					>
 						<ModuleCard className={`w-full`}>{item.name}</ModuleCard>
-					</div>
+					</motion.div>
 				);
 			})}
 			{children}
-		</div>
+		</motion.div>
 	);
 }
 
