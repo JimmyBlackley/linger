907c1eef27e2f25bf0bd9c5e32d0ef570dfa8b69
RyanMai <hoangmai1070@gmail.com>
2024-12-19 17:11:33 +1100

    removed framer motion animation listener to fix bug

--- src/ui/app/ContentArea.tsx
+++ src/ui/app/ContentArea.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ModuleCard from "./ModuleCard";
 import { v4 as uuidv4 } from "uuid";
 import { motion } from "motion/react";
@@ -19,21 +19,29 @@ function ContentArea({ children }: Props): JSX.Element {
 		{ id: uuidv4(), name: "Yet another test content" },
 	]);
 	const [currentDragItem, setCurrentDragItem] = useState<Item | null>(null);
-	const [animating, setAnimating] = useState(false);
 	/* function as dragStart because browser copy current element to use as drag ghost when dragStart procs */
 	function drag(e: MouseEvent | TouchEvent | PointerEvent, item: Item) {
 		setCurrentDragItem(item);
 	}
+	let lastDragOverTime = useRef(0);
+
 	/* switching items on drag over */
 	function dragOver(e: React.DragEvent, item: Item) {
-		if (currentDragItem && !animating) {
-			setAnimating(true);
-			const currentDragItemIndex = content.findIndex((arrayItem) => arrayItem.id === currentDragItem.id);
-			const itemToReplaceIndex = content.findIndex((arrayItem) => arrayItem.id === item.id);
-			const newContent = [...content];
-			const [removedItem] = newContent.splice(currentDragItemIndex, 1);
-			newContent.splice(itemToReplaceIndex, 0, removedItem);
-			setContent(newContent);
+		/* prevent dragOver animation from overlapping */
+		if (Date.now() - lastDragOverTime.current < 200) {
+			return;
+		}
+		lastDragOverTime.current = Date.now();
+
+		if (currentDragItem) {
+			setContent((content) => {
+				const currentDragItemIndex = content.findIndex((arrayItem) => arrayItem.id === currentDragItem.id);
+				const itemToReplaceIndex = content.findIndex((arrayItem) => arrayItem.id === item.id);
+				const newContent = [...content];
+				const [removedItem] = newContent.splice(currentDragItemIndex, 1);
+				newContent.splice(itemToReplaceIndex, 0, removedItem);
+				return newContent;
+			});
 		}
 	}
 	function dragEnd() {
@@ -67,9 +75,6 @@ function ContentArea({ children }: Props): JSX.Element {
 							dragOver(e, item);
 						}}
 						onDragEnd={dragEnd}
-						onLayoutAnimationComplete={() => {
-							setAnimating(false);
-						}}
 					>
 						<ModuleCard className={`w-full`}>{item.name}</ModuleCard>
 					</motion.div>
