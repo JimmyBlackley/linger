17d8673a9dd720c46470a838a2fbfe8f8ed0029c
RyanMai <hoangmai1070@gmail.com>
2024-12-19 15:27:34 +1100

    added drag and drop using dnd-kit libary

--- src/ui/app/ContentArea.tsx
+++ src/ui/app/ContentArea.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import ModuleCard from "./ModuleCard";
+import { SortableCard } from "./SortableCard";
 import { v4 as uuidv4 } from "uuid";
+import { closestCorners, DndContext, DragEndEvent } from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
 
 interface Props {
 	children?: React.ReactNode;
@@ -11,15 +13,33 @@ function ContentArea({ children }: Props): JSX.Element {
 		{ id: uuidv4(), name: "This is a module filled with content" },
 		{ id: uuidv4(), name: "Test content" },
 		{ id: uuidv4(), name: "Another test content" },
-		{ id: uuidv4(), name: "Yet another test content"}
+		{ id: uuidv4(), name: "Yet another test content" },
 	]);
+	function handleDragEnd(event: DragEndEvent) {
+		const { active, over } = event;
+		if (over && active.id !== over.id) {
+			setContent((content) => {
+				const oldIndex = content.findIndex((item) => item.id === active.id);
+				const newIndex = content.findIndex((item) => item.id === over.id);
+				return arrayMove(content, oldIndex, newIndex);
+			})
+		}
+	}
 	return (
-		<div className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto">
-			{content.map((item) => {
-				return <ModuleCard key={item.id} className={`w-full`}>{item.name}</ModuleCard>;
-			})}
-			{children}
-		</div>
+		<DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
+			<SortableContext items={content}>
+				<div className="grid grid-cols-3 auto-rows-min gap-3 p-3 overflow-y-auto">
+					{content.map((item) => {
+						return (
+							<SortableCard key={item.id} id={item.id} className={`w-full`}>
+								{item.name}
+							</SortableCard>
+						);
+					})}
+					{children}
+				</div>
+			</SortableContext>
+		</DndContext>
 	);
 }
 
---
+++ src/ui/app/SortableCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+interface Props {
+	className?: string;
+	children?: React.ReactNode;
+	id: string;
+}
+
+export function SortableCard({ className, children, id }: Props): JSX.Element {
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: id });
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
+
+	return (
+		<div
+			ref={setNodeRef}
+			style={style}
+			{...attributes}
+			{...listeners}
+			className={`flex w-[90%] h-[100px] items-center p-4 bg-[#d3cd85] rounded-[20px] gap-1 ${className}`}
+		>
+			<div className="w-20 h-20 bg-[#989898]" />
+			<p className="text-black text-lg flex-1 text-center">{children}</p>
+		</div>
+	);
+}
