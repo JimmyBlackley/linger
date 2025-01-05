7e0dc05811f5433a9186082f82c4eac41bb7dc4b
RyanMai <hoangmai1070@gmail.com>
2025-01-05 23:21:20 +1100

    fixed bug where dragging causes page to scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="overflow-hidden">
+    <html lang="en" className="overflow-hidden h-screen">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden`}
       >
         {children}
       </body>
