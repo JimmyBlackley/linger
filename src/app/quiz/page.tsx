9d6af0e254f7ba7062c203616cd0b0d4eb069508
James <james@Jamess-MacBook-Pro.local>
2025-01-15 14:34:47 +1100

    added new question types to db, added more seeds to db, time to intergrate

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,19 +1,22 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const App = () => {
   interface Quiz {
     id: number;
     title: string;
+    description?: string;
+    creatorId: number;
+    createdAt: string;
   }
 
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     const fetchQuizzes = async () => {
-      const response = await fetch('/api/quizzes');
+      const response = await fetch("/api/quizzes");
       const data = await response.json();
       setQuizzes(data);
     };
@@ -23,34 +26,42 @@ const App = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = await fetch('/api/quizzes', {
-      method: 'POST',
+    const creatorId = 1; // Hardcoded user ID
+    const response = await fetch("/api/quizzes", {
+      method: "POST",
       headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title }),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, creatorId }),
     });
     const newQuiz = await response.json();
     setQuizzes([...quizzes, newQuiz]);
-    setTitle('');
+    setTitle("");
   };
+
   return (
-    <div>
-      <h1>Quizzes</h1>
+    <div className="m-4">
+      <h1 className="text-[99px] text-black">Quizzes</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          placeholder="Quiz Title"
+          placeholder="Enter new quiz type"
           required
+          className="border text-black placeholder-slate-400"
         />
-        <button type="submit">Create Quiz</button>
+        <button
+          className="bg-white p-8 m-3 text-black border-black border"
+          type="submit"
+        >
+          Create Quiz
+        </button>
       </form>
-      <ul>
-        {quizzes.map((quiz) => (
-          <li key={quiz.id}>{quiz.title}</li>
-        ))}
+      <ul className="text-gray-950">
+        {quizzes.map((quiz) => {
+          return <li key={quiz.id}>{quiz.title}</li>;
+        })}
       </ul>
     </div>
   );
