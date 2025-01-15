8f57591a051325def087fe7832771d0210957c17
James <james@Jamess-MacBook-Pro.local>
2025-01-15 12:57:26 +1100

    testing new db endpoints

--- pages/api/quizzes.ts
+++ pages/api/quizzes.ts
@@ -2,10 +2,25 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../src/app/lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const quizzes = await prisma.quiz.findMany();
-    res.status(200).json(quizzes);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching quizzes' });
+  if (req.method === 'POST') {
+    const { title } = req.body;
+    try {
+      const newQuiz = await prisma.quiz.create({
+        data: { title },
+      });
+      res.status(201).json(newQuiz);
+    } catch (error) {
+      res.status(500).json({ error: 'Error creating quiz' });
+    }
+  } else if (req.method === 'GET') {
+    try {
+      const quizzes = await prisma.quiz.findMany();
+      res.status(200).json(quizzes);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching quizzes' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET', 'POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
