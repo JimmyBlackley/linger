19d3b297fdebcc5c95d43459276d15de3db5e2da
RyanMai <hoangmai1070@gmail.com>
2025-01-15 12:55:09 +1100

    styling

--- src/app/ui/app/DragOverlayCard.tsx
+++ src/app/ui/app/DragOverlayCard.tsx
@@ -8,9 +8,9 @@ interface Props {
 export function DragOverlayCard({ className, children}: Props): JSX.Element {
 	return (
 		<div
-			className={`flex w-[90%] h-[100px] items-center p-4 bg-[#d3cd85] shadow-lg rounded-[20px] gap-1 ${className}`}
+			className={`flex w-[90%] h-[100px] items-center p-4 bg-[#FFF3B0] shadow-lg rounded-[20px] gap-1 ${className}`}
 		>
-			<div className="w-20 h-20 bg-[#989898]" />
+			<div className="w-20 h-20 bg-[#E6DAFF]" />
 			<p className="text-black text-lg flex-1 text-center">{children}</p>
 		</div>
 	);
