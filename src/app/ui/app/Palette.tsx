13a13ee8e69f9ae33860def1b3e5c6a0a9a35553
RyanMai <hoangmai1070@gmail.com>
2025-01-17 12:59:39 +1100

    added moving sidebar

--- src/app/ui/app/Palette.tsx
+++ src/app/ui/app/Palette.tsx
@@ -14,45 +14,53 @@ interface Props {
 export function Palette({ className, moduleList, currentDragCard }: Props): JSX.Element {
 	const { setNodeRef } = useDroppable({ id: "palette" });
 	return (
-		<div className={`relative p-2 grid grid-cols-[2rem_1fr] grid-rows-[2rem_1fr] gap-2 bg-purple-600 ${className}`}>
+		<div className={`relative flex flex-col p-2 gap-2 bg-purple-600 ${className}`}>
+			{/* drag overlay */}
 			{currentDragCard?.data.current?.container === "timeline" && (
 				<div className="absolute inset-0 bg-red-500 opacity-50 pointer-events-none"></div>
 			)}
-			<motion.div className="row-span-2 flex flex-col gap-2">
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
+			{/* top bar */}
+			<motion.div className="flex gap-2 justify-between">
+				<MenuItem className="text-2xl">Home</MenuItem>
+				<div className="flex gap-2">
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+				</div>
 			</motion.div>
-
-			<motion.div className="flex justify-end gap-2">
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
-				<MenuItem className="text-2xl">+</MenuItem>
-			</motion.div>
-
-			<div
-				ref={setNodeRef}
-				className="grid grid-cols-3 auto-rows-min gap-3 p-3 h-full w-full bg-white rounded-[14px] overflow-y-auto"
-			>
-				<SortableContext items={moduleList}>
-					{moduleList.map((module) => {
-						return (
-							<SortableCard
-								key={module.id}
-								className={`w-full flex-shrink-0 ${
-									currentDragCard?.id === module.id ? "invisible" : ""
-								}`}
-								container="palette"
-								name={module.name}
-								id={module.id}
-							>
-								{module.name}
-							</SortableCard>
-						);
-					})}
-				</SortableContext>
+			<div className="flex gap-2 h-full">
+				{/* side bar */}
+				<motion.div
+					className="flex flex-col gap-2 w-8 hover:w-[18rem] transition-all duration-300"
+				>
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+					<MenuItem className="text-2xl">+</MenuItem>
+				</motion.div>
+				<div
+					ref={setNodeRef}
+					className="grid grid-cols-3 auto-rows-min w-full h-full gap-3 p-3 bg-white rounded-[14px] overflow-y-auto"
+				>
+					<SortableContext items={moduleList}>
+						{moduleList.map((module) => {
+							return (
+								<SortableCard
+									key={module.id}
+									className={`w-full flex-shrink-0 ${
+										currentDragCard?.id === module.id ? "invisible" : ""
+									}`}
+									container="palette"
+									name={module.name}
+									id={module.id}
+								>
+									{module.name}
+								</SortableCard>
+							);
+						})}
+					</SortableContext>
+				</div>
 			</div>
 		</div>
 	);
