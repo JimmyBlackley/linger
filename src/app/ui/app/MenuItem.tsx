3ab0e535804ff0cbd3789357fa25b86c902e345a
RyanMai <hoangmai1070@gmail.com>
2025-02-04 19:00:50 +0700

    added text to sidebar with new MenuItem

--- src/app/ui/app/MenuItem.tsx
+++ src/app/ui/app/MenuItem.tsx
@@ -1,10 +1,40 @@
+import { motion } from "motion/react";
 import React from "react";
 
 interface Props {
 	children?: React.ReactNode;
 	className?: string;
+	onClick?: () => void;
 }
 
-export function MenuItem({ children, className}: Props): JSX.Element {
-	return <button className={`w-8 h-8 rounded-md ${className}`}>{children}</button>;
+export function MenuItem({ children, className, onClick}: Props): JSX.Element {
+	const [isActive, setIsActive] = React.useState(false);
+
+	return (
+		<button
+			type="button"
+			/* set to active on click */
+			onMouseDown={() => {
+				setIsActive(true);
+			}}
+			onMouseUp={() => {
+				setIsActive(false);
+			}}
+			onClick={onClick}
+			className={`relative ${className}`}
+		>
+			<motion.div
+				variants={{
+					default: { opacity: 0 },
+					hover: { opacity: 0.16 },
+					active: { opacity: 0.22 },
+				}}
+				initial="default"
+				whileHover={isActive ? "active" : "hover"}
+				animate={isActive ? "active" : "default"}
+				className={`absolute z-30 top-0 left-0 w-full h-full rounded-[inherit] bg-white`}
+			></motion.div>
+			{children}
+		</button>
+	);
 }
